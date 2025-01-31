2746a96053b74c40921f7dbf1b437e61c2941257
Mangeni_Buheri <buherishalom@gmail.com>
2025-01-31 15:06:23 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,3 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // Search button functionality
-    const searchButton = document.querySelector(".search-bar button");
-    searchButton.addEventListener("click", function() {
-        alert("Search functionality is not yet implemented.");
-    });
-
-    // Newsletter subscription validation
-    const subscribeButton = document.querySelector(".newsletter button");
-    subscribeButton.addEventListener("click", function(event) {
-        event.preventDefault();
-        const emailInput = document.querySelector(".newsletter input[type='email']");
-        const email = emailInput.value;
-        const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
-
-        if (emailPattern.test(email)) {
-            alert("Thank you for subscribing!");
-        } else {
-            alert("Please enter a valid email address.");
-        }
-    });
+    // Example of JavaScript code if needed for further interactions
 });
